b0b720c76d4e6cc558c31e0f05293b9f16f4f2a8
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-05-12 17:45:43 -0600

    feat: wip criação de validação de CPF

---
+++ SaaSVet/Contexts/Auth/Domain/ValueObjects/cpf.cs
@@ -0,0 +1,30 @@
+﻿namespace SaaSVet.Contexts.Auth.Domain.ValueObjects;
+
+public class cpf
+{
+    public string value { get; set; }
+
+    public cpf(string value)
+    {
+        if (value.Length != 11)
+            cpfInvalido();
+        
+        
+            
+    }
+
+    public void cpfInvalido()
+    {
+        throw new ArgumentException("CPF inválido");
+    }
+
+    public bool verificarJ(string valor)
+    { 
+        throw new NotImplementedException("Ainda não implementado");
+    }
+
+    public bool vreificarK(string valor)
+    {
+        throw new NotImplementedException("Ainda não implementado");
+    }
+}
