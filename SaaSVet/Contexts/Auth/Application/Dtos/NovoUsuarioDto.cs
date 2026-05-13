94c20f58d043243914650c81306dd10d6d369693
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-05-12 19:31:49 -0600

    feat: novo usecase para a criacao de usuario

---
+++ SaaSVet/Contexts/Auth/Application/Dtos/NovoUsuarioDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SaaSVet.Contexts.Auth.Application.Dtos;
+
+public record NovoUsuarioDto (
+    string nome,
+    string email,
+    string senha
+    );
---
+++ SaaSVet/Contexts/Auth/Application/NovoUsuarioUseCase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SaaSVet.Contexts.Auth.Application.Dtos;
+using SaaSVet.Contexts.Auth.Domain.Entities;
+
+namespace SaaSVet.Contexts.Auth.Application;
+
+public class NovoUsuarioUseCase(UserManager<Usuario> _userManager)
+{
+    public async Task Run(NovoUsuarioDto dto)
+    {
+        Usuario novoUsuario = new Usuario {
+            UserName = dto.email,
+            nome = dto.nome,
+            Email =  dto.email,
+        };
+        
+        var result = await _userManager.CreateAsync(novoUsuario, dto.senha);
+        
+        if (result.Succeeded)
+            await _userManager.AddToRoleAsync(novoUsuario, "Usuario");
+
+        else
+            throw new  Exception(result.Errors.First().Description);
+    }
+}
