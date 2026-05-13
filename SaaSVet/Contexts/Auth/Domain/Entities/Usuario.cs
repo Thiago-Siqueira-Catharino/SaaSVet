fccddd7ad0c3af1826a5bd7ab1733cca0022a872
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-05-12 18:26:08 -0600

    build: nova entidade de dominio

---
+++ SaaSVet/Contexts/Auth/Domain/Entities/Usuario.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SaaSVet.Contexts.Auth.Domain.ValueObjects;
+
+namespace SaaSVet.Contexts.Auth.Domain.Entities;
+
+public class Usuario : IdentityUser
+{
+    public string nome { get; set; }
+    public cpf cpf { get; set; }
+}
