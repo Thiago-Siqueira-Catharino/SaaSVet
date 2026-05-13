86dfbc7795519e6504cd054434a6caa7090bb2f9
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-05-12 19:00:50 -0600

    build: auth controller setup

---
+++ SaaSVet/Contexts/Auth/Presentation/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaaSVet.Contexts.Auth.Presentation;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController() : ControllerBase
+{
+    [HttpGet("/ping")]
+    public IActionResult GetPing()
+    {
+        return Ok("pong");
+    }
+}
