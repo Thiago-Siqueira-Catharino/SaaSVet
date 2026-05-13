8bf508fa7b4fcc280e49e9d4e8f47d9b2c80f45b
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-05-12 18:45:31 -0600

    feat: dbcontext setup, com adição de dependecias necessarias

---
+++ SaaSVet/Contexts/Auth/Infrastructure/Persistance/AuthDbContext.cs
@@ -0,0 +1,14 @@
+﻿using SaaSVet.Contexts.Auth.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SaaSVet.Contexts.Auth.Infrastructure.Persistance;
+
+public class AuthDbContext : IdentityDbContext<Usuario>
+{
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+    }
+}
