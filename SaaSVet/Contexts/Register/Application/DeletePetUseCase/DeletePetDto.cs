9acc39d07d6e9719240e79983092a07ae7e77aa4
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-05-26 18:42:22 -0600

    feat: new usecase to remove pets

---
+++ SaaSVet/Contexts/Register/Application/DeletePetUseCase/DeletePetDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SaaSVet.Contexts.Register.Application.DeletePetUseCase;
+
+public record DeletePetDto
+{
+    public int ownerId { get; set; }
+    public int petId { get; set; }
+};
---
+++ SaaSVet/Contexts/Register/Application/DeletePetUseCase/DeletePetUseCase.cs
@@ -0,0 +1,29 @@
+﻿using SaaSVet.Contexts.Register.Domain.IRepositories;
+
+namespace SaaSVet.Contexts.Register.Application.DeletePetUseCase;
+
+public class DeletePetUseCase
+{
+    private readonly IPetOwnerRepository _petOwnerRepository;
+    private readonly IPetRepository _petRepository;
+
+    public DeletePetUseCase(IPetOwnerRepository petOwnerRepository, IPetRepository petRepository)
+    {
+        _petOwnerRepository = petOwnerRepository;
+        _petRepository = petRepository;
+    }
+
+    public async Task RunAsync(DeletePetDto dto)
+    {
+        var owner = await _petOwnerRepository.FindByIdAsync(dto.ownerId);
+        if (owner == null)
+            throw new Exception("Owner not found");
+        
+        var pet = owner.pets.Where(pet => pet.id == dto.petId).FirstOrDefault();
+        if (pet == null)
+            throw new Exception("Pet not found");
+        
+        owner.pets.Remove(pet);
+        await _petOwnerRepository.SaveAsync(owner);
+    }
+}
