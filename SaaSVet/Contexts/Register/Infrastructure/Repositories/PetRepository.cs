25ee4cfc4c7dd1c7d83cbddd3a0a38bbd32feb57
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-05-26 18:43:57 -0600

    feat: repositories implementation

---
+++ SaaSVet/Contexts/Register/Infrastructure/Repositories/PetRepository.cs
@@ -0,0 +1,20 @@
+﻿using SaaSVet.Contexts.Register.Domain.Entities;
+using SaaSVet.Contexts.Register.Domain.IRepositories;
+using SaaSVet.Contexts.Register.Infrastructure.Persistance;
+
+namespace SaaSVet.Contexts.Register.Infrastructure.Repositories;
+
+public class PetRepository : IPetRepository
+{
+    public readonly AppointmentsDbContext _database;
+    public PetRepository(AppointmentsDbContext database)
+    {
+        _database = database;
+    }
+
+    public async Task AddAsync(Pet pet)
+    {
+        await  _database.Pets.AddAsync(pet);
+        await _database.SaveChangesAsync();
+    }
+}
