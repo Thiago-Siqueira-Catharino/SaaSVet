94c7c035bc66491b96b3306503a9c9e49b2aaf7a
ThiagoSiqueira <thiago.s.catharino@gmail.com>
2026-06-09 16:41:30 +0000

    feat: new controller for pet owners (#7)

---
+++ SaaSVet/Contexts/Register/Presentation/OwnerController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using SaaSVet.Contexts.Register.Application.NewOwnerUseCase;
+
+namespace SaaSVet.Contexts.Register.Presentation;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OwnerController (NewOwnerUseCase newOwnerUseCase) : ControllerBase
+{
+    [HttpPost("/add")]
+    public async Task<IActionResult> AddOwner(NewOwnerDto dto)
+    {
+        try
+        {
+            await newOwnerUseCase.RunAsync(dto);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest(e.Message);
+        }
+    }
+    
+}
