d2264c1f114317ca5cd730066a1951d9f9d28ba5
Thiago Siqueira Catharino <thiago.s.catharino@gmail.com>
2026-06-09 12:27:09 -0300

    feat: add repository interface

---
+++ SaaSVet/Contexts/Appoitment/Domain/IRepositories/IAppointmentRepository.cs
@@ -0,0 +1,13 @@
+using SaaSVet.Contexts.Appoitment.Domain.Entities;
+
+namespace SaaSVet.Contexts.Appoitment.Domain.IRepositories;
+
+public interface IAppointmentRepository
+{
+    public Task AddAsync(Appointment appointment);
+    public Task SaveAsync(Appointment appointment);
+    public Task<bool> ExistsConflicAsync(int petId, DateTime windowStart, DateTime windowEnd);
+    public Task<Appointment> GetByIdAsync(int id);
+    public Task<List<Appointment>> GetByPetIdAsync(int petId);
+    
+}
